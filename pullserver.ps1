352de24bb504057f4fd6b61d5388960c96979724
gabriel <gabriel@cloudconfusion.co.uk>
2017-08-06 15:33:57 +0100

    added comments and links for certificates

--- pullserver.ps1
+++ pullserver.ps1
@@ -1,4 +1,9 @@
 # https://docs.microsoft.com/en-us/powershell/dsc/pullserver
+#https://blogs.msdn.microsoft.com/powershell/2015/10/01/powershell-dsc-faq-sorting-out-certificates/
+#http://blog.superautomation.co.uk/2016/12/how-to-create-certificate-template-for.html
+#you'll need to install the dsc modules to set this up
+#the standard ps respository should already be set up but in case it isn't 
+Register-PSrepository -default #thi should error in most cases saying it already exists
 
-Register-PSrepository -Name "psgallery" -SourceLocation "https://www.powershellgallery.com/api/v2/"
+#you'll need to be running this as admin to install a module
 Install-Module -Name xPSDesiredStateConfiguration -Verbose -Force -AllowClobber
