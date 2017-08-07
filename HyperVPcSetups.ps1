703a33a3621f15672d7aa2235d8514f921c5d79e
Gabriel McColl <GMcColl@martincurrie.com>
2017-08-07 13:34:25 +0100

    finished creating the Switch

--- HyperVPcSetups.ps1
+++ HyperVPcSetups.ps1
@@ -1,7 +1,18 @@
 #Powershell to setup the VMs in Hyper V, basic desktop host hypervisor with 1 Network card.
+#REMEMBER YOU'LL NEED TO START YOUR IDE AS ADMIN 
 
 $ServerOSIso =  "C:\ISO\Server2012R2.ISO" #location of your Server2016 (or 2012R2)iso
 $VMandVHDlocation = "U:\VirtualMachines\" #where you're going to store the VMsand their Harddisks
+$Hypervisor = "localhost" #change this if you're setting it up on a remote PC
+
+#https://technet.microsoft.com/en-us/itpro/powershell/windows/hyper-v/new-vmswitch
+#CREATE THE VIRTUAL SWITCH
+$SwitchName = "External" #change these if you like.
+$SwitchType = "" #this doesn't need set as we are using the netadaptername parameter which auto makes it External
+$SwitchNote = "created by script"
+$networkcardtobind = "Ethernet 2" #get this by running get-netadapter and using the name of the one you want
+
+New-VMSwitch -ComputerName $Hypervisor -Name $SwitchName -NetAdapterName $networkcardtobind -AllowManagementOS $true -note $SwitchNote
 
 
 #Pick the domain name, and then what your VMs will be prefixed and suffixed with
