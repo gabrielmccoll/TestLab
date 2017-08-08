3c88edb0555ebee7d707143304acd304a875f80b
Gabriel McColl <GMcColl@martincurrie.com>
2017-08-08 15:24:15 +0100

    latest findings

--- ModifyUnattend.ps1
+++ ModifyUnattend.ps1
@@ -1,6 +1,22 @@
 #https://blogs.msdn.microsoft.com/sonam_rastogi_blogs/2014/05/14/update-xml-file-using-powershell/
 
-#$xml.unattend.settings.selectnodes("*")|where {$_.name -like "*shell*"}
+<#This works to set it
+#($xml.unattend.settings.selectnodes("*")|where {$_.ProductKey -gt 1}).ProductKey = "1233"
+running this doesn't tho
+$xml.unattend.settings.component.productkey | Get-Member   
+Comesback as TypeName: System.String
+
+$xml.unattend.settings.component | Get-Member
+   TypeName: System.Xml.XmlElement#urn:schemas-microsoft-com:unattend#component
+Doesn't have a Product Key Method or Parameter listed so that's why you can't set it that way !
+
+
+This one below  works as it has a ProductKey Parameter you can set even though seems same Objecttype??
+($xml.unattend.settings.selectnodes("*")|where {$_.ProductKey -gt 1}) |Get-Member
+Comes back as TypeName: System.Xml.XmlElement#urn:schemas-microsoft-com:unattend#component
+Because you're selecting the node, then you're editing a property of the node this works. 
+#>
+
 
 
 $ProductKey = "GGGGN-FT8W3-Y4M27-J84CP-Q3VJ9"
@@ -10,8 +26,8 @@ $Organization = "TestLandia2"
 $Owner = "Boss"
 $ComputerName = "PCname1"
 
-$path = "C:\Users\breau\TestLab\Autounattendtest.xml"
-$newfile = "C:\Users\breau\TestLab\Autounattendtest2.xml"
+$path = "$env:USERPROFILE\TestLab\Autounattendtest.xml"
+$newfile = "$env:USERPROFILE\TestLab\Autounattendtest2.xml"
 $xml = [xml](Get-Content $path)
 $xmlcomponent = $xml.unattend.settings.component
 
