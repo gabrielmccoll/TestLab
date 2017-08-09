927d43b42069c8869a6a25c5be2ded4c845aaba4
Gabriel McColl <GMcColl@martincurrie.com>
2017-08-09 17:28:46 +0100

    make pull server

--- Pullserver.ps1
+++ Pullserver.ps1
@@ -1,6 +1,7 @@
 # https://docs.microsoft.com/en-us/powershell/dsc/pullserver
 #https://blogs.msdn.microsoft.com/powershell/2015/10/01/powershell-dsc-faq-sorting-out-certificates/
 #http://blog.superautomation.co.uk/2016/12/how-to-create-certificate-template-for.html
+#https://www.starwindsoftware.com/blog/how-to-build-a-secure-powershell-dsc-pull-server
 #you'll need to install the dsc modules to set this up
 #the standard ps respository should already be set up but in case it isn't 
 Register-PSrepository -default #thi should error in most cases saying it already exists
