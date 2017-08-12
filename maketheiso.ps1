f45aad85dd3f3f1ca9670018d8d7ac832621b53d
gabriel <gabriel@cloudconfusion.co.uk>
2017-08-12 11:41:40 +0100

    saved make iso

--- maketheiso.ps1
+++ maketheiso.ps1
@@ -2,4 +2,8 @@
 $filesforiso = "C:\ISOs\6mnth2016ext"
 $newiso = "C:\ISOs\6mnth2016GMMOD.iso"
 
-@dir $filesforiso | New-IsoFile -Path $newiso -BootFile "$filesforiso\boot\etfsboot.com" -Media DVDPLUSR -Title "Win2016mod6mo"
+dir $filesforiso | New-IsoFile -Path $newiso -BootFile "$filesforiso\boot\etfsboot.com" -Media DVDPLUSR -Title "Win2016mod6mo"
+$filesforiso = "C:\ISOs\6mnth2016ext"
+$newiso = "C:\ISOs\6mnth2016GMMOD.iso"
+
+dir $filesforiso | New-IsoFile -Path $newiso -BootFile "$filesforiso\boot\etfsboot.com" -Media DVDPLUSR -Title "Win2016mod6mo"
