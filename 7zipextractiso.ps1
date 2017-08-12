9f8e9aee11b36f5cce47708f908806a24dd67101
gabriel <gabriel@cloudconfusion.co.uk>
2017-08-12 11:41:07 +0100

    first almost working

--- 7zipextractiso.ps1
+++ 7zipextractiso.ps1
@@ -2,4 +2,4 @@ $isoloc ="C:\ISOs\6mnth2016.ISO"
 $isoOut ="C:\ISOs\6mnth2016ext"
 
 start-process -FilePath "$env:ProgramFiles\7-zip\7z.exe" `
--ArgumentList "e $isoloc -o $isoOut"
+-ArgumentList "x $isoloc -o$isoOut -y"
---
+++ maketheiso.ps1
@@ -0,0 +1,5 @@
+
+$filesforiso = "C:\ISOs\6mnth2016ext"
+$newiso = "C:\ISOs\6mnth2016GMMOD.iso"
+
+@dir $filesforiso | New-IsoFile -Path $newiso -BootFile "$filesforiso\boot\etfsboot.com" -Media DVDPLUSR -Title "Win2016mod6mo"
